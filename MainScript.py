e2646feb628ad452340ce149eb34c1dd856d5f56
StefanoFochesatto <stefano.gfochesatto@gmail.com>
2022-06-07 19:27:37 -0800

    large refactor

--- MainScript.py
+++ MainScript.py
@@ -2,6 +2,7 @@
 
 #################### Dependencies #################### 
 # For Data Management
+from http.server import ThreadingHTTPServer
 import numpy as np
 import pandas as pd
 # For Directory Management
@@ -14,16 +15,19 @@ import copy
 
 # For Optical Character Recognition
 import easyocr
-from scipy.fft import dstn
 from datetime import datetime 
 from datetime import timedelta
-
 
 # For Timing Function 
 import time
 
 # For Sunrise/Sunset Flag
 from suntime import Sun, SunTimeException
+
+# For threshold comparison and snowcover calculations
+from skimage.metrics import normalized_root_mse
+import math
+
 
 latitude = 64.95156082750202
 longitude = -147.6210085622378
@@ -33,27 +37,30 @@ sun = Sun(latitude, longitude)
 #################### Global Variables #################### 
 ## Supply Full Path to TimeLapse directory
 # path = "/Users/stefanofochesatto/Desktop/CRREL-Solar-Project/TestTimeLapse" 
-path = r"C:\Users\Amanda Barker\Desktop\Stefano\CRREL-Solar-Project\TestTimeLapse"
+path = r"C:\Users\AKROStudent2\Desktop\CRREL-Solar-Project\TestTimeLapse"
 reader = easyocr.Reader(['en']) # Initializing OCR Engine
-MaskCoordinates = [] # User defined Mask Coordinated global variable. 
-PanelDictionary = {
+PanelDictionary = { 
     '2':[10, 8, 2, 5],
     '1':[1, 4, 7, 11], 
     '3':[9, 3, 6, 12],
     'A':['all', 'all', 'all', 'all']
 }
-
+GenerateVid = True
 
 
 
 ## Initilizing Python lists to store our data
+MaskCoordinates = [] # User defined Mask Coordinated global variable. 
 TimeStampBuffer = []
 TimeStamp = []
 SnowCover = []
+prevThresh = []
+
+
 # Testing video
 imgArray = []
 SunFlag = []
-prevThresh = []
+
 
 
 
@@ -95,7 +102,6 @@ def FindThreshold(dst, mask):
     return thresh
 
 
-
 def OCRPreProcess(img):
         img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
         # Adding Border
@@ -109,10 +115,6 @@ def OCRPreProcess(img):
         # Applying Blur
         img = cv2.threshold(cv2.medianBlur(img, 1), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
         return img
-
-
-
-
 
 
 def SunriseFlag(CurrentTime):
@@ -135,16 +137,6 @@ def SunriseFlag(CurrentTime):
         SunFlag.append(1)
     else:
         SunFlag.append(0)
-
-
-
-
-
-
-
-
-
-
 
 
 def TimeStampCollation(img):
@@ -176,7 +168,6 @@ def TimeStampCollation(img):
         TimeStamp.append(TimeStamp[-1] + TimestampDifference)
         
 
-
 def ExtractPanelID(img):
     PanelID = img[0:60,835:860]
     PanelID = OCRPreProcess(PanelID)
@@ -185,87 +176,38 @@ def ExtractPanelID(img):
     return PanelIDText[0]
 
 
+def GetPrevThresh(PanelNumber,kernel):
+    if (kernel == 0):
+        ThresholdsByPanel = prevThresh
+        ThreshList = [i[PanelNumber] for i in ThresholdsByPanel]
+        return ThreshList
+    else:
+        ThresholdsByPanel = prevThresh[-kernel:-1]
+        ThreshList = [i[PanelNumber] for i in ThresholdsByPanel]
+        return ThreshList
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
+    w_min = min(im.shape[1] for im in im_list)
+    im_list_resize = [cv2.resize(im, (w_min, int(im.shape[0] * w_min / im.shape[1])), interpolation=interpolation)
+                      for im in im_list]
+    return cv2.vconcat(im_list_resize)
 
 
 def ImageProcess(img, PanelID):
     TimeStampCollation(img)
     SunriseFlag(TimeStamp[-1])
-   # ############ REFACTOR THIS ##################################################
-   # #########################################################################################################
-   # global TimeStamp
-   # global TimeStampBuffer
-   # ## Add PSA Classification
-   # if (len(TimeStampBuffer) < 2):
-   #     ####### Extracting Timestamp Data with easyOCR ####### 
-   #     TimeStampCrop = img[0:60,0:620]
-   #     PanelID = img[0:60,620:860]
-   #     ## Preprocessing ##
-   #     # Resizing
-   #     TimeStampCrop = cv2.resize(TimeStampCrop, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-   #     # Adding Border
-   #     TimeStampCrop = cv2.copyMakeBorder(TimeStampCrop, 80, 80, 80, 80, cv2.BORDER_CONSTANT, value=[0, 0, 0])
-   #     # Converting to GrayScale
-   #     TimeStampCrop = cv2.cvtColor(TimeStampCrop, cv2.COLOR_BGR2GRAY)
-   #     # Dilation and Erosion
-   #     kernel = np.ones((1, 1), np.uint8)
-   #     TimeStampCrop = cv2.dilate(TimeStampCrop, kernel, iterations=10)
-   #     TimeStampCrop = cv2.erode(TimeStampCrop, kernel, iterations=1)
-   #     # Applying Blur
-   #     TimeStampCrop = cv2.threshold(cv2.medianBlur(TimeStampCrop, 1), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-   #
-   #     ### Running OCR engine on image ###
-   #     print('Running OCR')
-   #     TimingOCR = time.time()
-   #     TimeStampText = reader.readtext(TimeStampCrop, allowlist = '0123456789:- ', width_ths=1)
-   #     TimeStampText = [val[1] for val in TimeStampText]
-   #     TimingOCRELAPSED = time.time() - TimingOCR
-   #     print('Frame TimeStamp: ',TimeStampText)
-   #     print('OCR Processing took: ',str(TimingOCRELAPSED))
-   #     
-   #     ### Updating Global Data Lists ###
-   #     CurrentTimeStamp = datetime.strptime(TimeStampText[0][0:19], "%Y-%m-%d %H:%M:%S")
-   #     print(CurrentTimeStamp)
-   #     TimeStamp.append(CurrentTimeStamp)
-   #     TimeStampBuffer.append(CurrentTimeStamp)
-   # else:    
-   #     TimestampDifference =  TimeStamp[1] - TimeStamp[0]
-   #     print(TimeStamp[-1] + TimestampDifference)
-   #     TimeStamp.append(TimeStamp[-1] + TimestampDifference)
-   # ############ REFACTOR THIS ##################################################
-   # #########################################################################################################
-
-
-
     SnowCoverFrame = [PanelID]
+    prevThreshFrame = []
+    
+
+    if (GenerateVid):
+        PanelVidArray = []
+
     ####### Extracting SnowCover Data with User Defined Masks #######
     ### Cropping the Panels ###
     print('Running Image Processing')
     TimingImageProcess = time.time()
-
-
-
-    sampleintensity = img[0:60,890:910]
-    sampleintensity = cv2.cvtColor(sampleintensity, cv2.COLOR_BGR2GRAY)
-    sampleintensity = cv2.mean(sampleintensity)[0]
     for i in range(0,len(MaskCoordinates),4):
         pts = np.array([MaskCoordinates[i],MaskCoordinates[i+1],MaskCoordinates[i+2],MaskCoordinates[i+3]])
         
@@ -274,10 +216,6 @@ def ImageProcess(img, PanelID):
         x,y,w,h = rect
         cropped = img[y:y+h, x:x+w].copy()
 
-        ## Converting the crop to gray scale
-        #cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-
-
         ## Generating the Mask
         pts = pts - pts.min(axis=0)
         mask = np.zeros(cropped.shape[:2], np.uint8)
@@ -285,79 +223,93 @@ def ImageProcess(img, PanelID):
 
         ## Removing everything outside the Mask with bitwise operation
         dst = cv2.bitwise_and(cropped, cropped, mask=mask)
-        ## Setting background to gray (Helps with Nightime Thresholding)
+        ## Setting background to black (is)
         bg = np.ones_like(cropped, np.uint8)*0
         cv2.bitwise_not(bg,bg, mask=mask)
         dst = bg+dst
-        ### THIS IS VERY IMPORTANT IT DEALS WITH THE LINES AND DOTS ON THE PANELS AND IS The Main Preprocessing ###
-        dst = cv2.medianBlur(dst, ksize=7)
+
+        ### THIS IS VERY IMPORTANT!!!!! It is The Main Preprocessing ###
+        dst = cv2.medianBlur(dst, ksize=9)
         dst = cv2.bilateralFilter(dst,9,75,75)
 
-        lab = cv2.cvtColor(dst, cv2.COLOR_BGR2LAB)
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-        lab[...,0] = clahe.apply(lab[...,0])
-        dst = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
-        frame_HSV = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
-        frame_threshold = cv2.inRange(frame_HSV, (0, 0, 90), (180, 18, 255))
+
+        # lab = cv2.cvtColor(dst, cv2.COLOR_BGR2LAB)
+        # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        # lab[...,0] = clahe.apply(lab[...,0])
+        # dst = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
         #dst = cv2.GaussianBlur(dst,(5,5),20)
         #print(int(sampleintensity/5))
         #M = np.ones(dst.shape,  dtype="uint8") * int(sampleintensity/5)
         #dst = cv2.add(dst, M)
-
         #print(int(sampleintensity/5))
 
+        ## Computing HSV Threshold (Color Threshold) ##
+        frame_HSV = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
+        HSV_Threshold = cv2.inRange(frame_HSV, (0, 0, 90), (180, 15, 255)) ## HSV Range for White/Greyish
 
 
-        ## Minimal threshold
+        ## Computing Otsu Threshold (Intensity Threshold) ##
+        dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
         Thresh = FindThreshold(dst, mask)
-        prevThresh.append(Thresh)
+        prevThreshFrame.append(Thresh)
         print(Thresh)
+
+        ## Gaussian Kernel Smoothing, on Otsu thresholds (kinda)##
         if(len(prevThresh) >= 6):
-            CurrentMean = np.mean(prevThresh[-6:-1])
-            CurrentStd = np.std(prevThresh[-6:-1])
+            ThreshList = GetPrevThresh(int(i/4), 6) 
+            CurrentMean = np.mean(ThreshList)
+            CurrentStd = np.std(ThreshList)
 
-            if (Thresh < CurrentMean - .5*CurrentStd or Thresh > CurrentMean - .5*CurrentStd):
+            if (Thresh < CurrentMean - .5*CurrentStd or Thresh > CurrentMean + .5*CurrentStd):
                 Thresh = CurrentMean
-                prevThresh[-1] = Thresh
+                
         else:
-            CurrentMean = np.mean(prevThresh)
-            CurrentStd = np.std(prevThresh)
+            ThreshList = GetPrevThresh(int(i/4), 0) 
+            CurrentMean = np.mean(ThreshList)
+            CurrentStd = np.std(ThreshList)
 
-            if (Thresh < CurrentMean - .5*CurrentStd or Thresh > CurrentMean - .5*CurrentStd):
+            if (Thresh < CurrentMean - .5*CurrentStd or Thresh > CurrentMean + .5*CurrentStd):
                 Thresh = CurrentMean
-                prevThresh[-1] = Thresh
 
-        if (Thresh <= 50):
-            Thresh = 120
+        Otsu_Threshold = cv2.threshold(dst,Thresh,255,cv2.THRESH_BINARY)[1]
+        
+        ## Comparing Both SnowCover Measurements ##
+        score = normalized_root_mse(Otsu_Threshold, HSV_Threshold)
+        global SnowCover
+        if (score > .90 and len(SnowCover) > 0):
+            HSVSnow = math.floor((1 - ((len(np.extract(mask > 0, HSV_Threshold)) - np.count_nonzero(np.extract(mask > 0, HSV_Threshold)))/len(np.extract(mask > 0, HSV_Threshold))))*100)/100
+            OTSUSnow = math.floor((1 - ((len(np.extract(mask > 0, Otsu_Threshold)) - np.count_nonzero(np.extract(mask > 0, Otsu_Threshold)))/len(np.extract(mask > 0, Otsu_Threshold))))*100)/100
+            if (len(SnowCover) < 5):
+                SnowCoverKernel = SnowCover
+            else:        
+                SnowCoverKernel = SnowCover[-5:-1]
 
+            SnowCoverKernelList1 = [j[int(i/4)+1] for j in SnowCoverKernel]
+            SnowCoverKernelList = [j[1] for j in SnowCoverKernelList1]
+            meanSnowCover = np.mean(SnowCoverKernelList)
+            if (abs(HSVSnow - meanSnowCover) > abs(OTSUSnow - meanSnowCover)):
+                Final = Otsu_Threshold
+            else:
+                Final = HSV_Threshold
 
+        elif (len(SnowCover) == 0):
+            Final = HSV_Threshold
+        else:
+            Final = Otsu_Threshold
 
-
-
-
-        #print(Thresh,(70 - int(sampleintensity/2)))
-
-        print(Thresh)
-        # prevThresh.append(Thresh)
-
-        ### Applying Threshold
-        ret3,th3 = cv2.threshold(dst,Thresh,255,cv2.THRESH_BINARY)
-        ## Pixel math for SnowCover
-        cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-        th3 = frame_threshold
-        SnowCoverPercentage = 1 - ((len(np.extract(mask > 0, th3)) - np.count_nonzero(np.extract(mask > 0, th3)))/len(np.extract(mask > 0, th3)))
+        SnowCoverPercentage = math.floor((1 - ((len(np.extract(mask > 0, Final)) - np.count_nonzero(np.extract(mask > 0, Final)))/len(np.extract(mask > 0, Final))))*100)/100
         SnowCoverFrame.append((PanelDictionary[PanelID][int(i/4)], SnowCoverPercentage))
 
+        if (GenerateVid):
+            cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+            PanelVidArray.append(cv2.hconcat([cropped, HSV_Threshold, Otsu_Threshold, Final, dst]))
+    
+    if (GenerateVid):
+        global imgArray 
+        imgArray.append(vconcat_resize_min(PanelVidArray))
 
 
-        testFrame = th3
-        global imgArray
-        imgArray.append(testFrame)
-
-
-
-
-    global SnowCover
+    prevThresh.append(prevThreshFrame)
     SnowCover.append(SnowCoverFrame)
     TimingImageProcessELAPSED = time.time() - TimingImageProcess
     print('Image Processing took: ', str(TimingImageProcessELAPSED))
@@ -448,12 +400,15 @@ if __name__ == "__main__":
     data = {'TimeStamp':TimeStamp, 'SnowCover':SnowCover}
     df = pd.DataFrame(data)
     df.to_csv('test.csv', index=False)
-    height, width = imgArray[0].shape
-    size = (width,height)
-    out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, size, 0)
-    for i in range(len(imgArray)):
-        out.write(imgArray[i])
-    out.release()
+
+
+    if (GenerateVid):
+        height, width = imgArray[0].shape
+        size = (width,height)
+        out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, size, 0)
+        for i in range(len(imgArray)):
+            out.write(imgArray[i])
+        out.release()
 
     
 
